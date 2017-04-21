df28595f09487c95196324cde60ecc6b1ec87c47
jeneser <jeneserwang@gmail.com>
2017-04-21 23:42:03 +0800

    Fix Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ var app = express()
 
 module.exports = require('./config/express')(app, config)
 
-app.all('/user', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
-
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port)
 })
--- config/express.js
+++ config/express.js
@@ -1,62 +1,70 @@
-var express = require('express');
-var glob = require('glob');
+var express = require('express')
+var glob = require('glob')
 
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compress = require('compression');
-var methodOverride = require('method-override');
+// var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var compress = require('compression')
+var methodOverride = require('method-override')
 
-module.exports = function(app, config) {
-  var env = process.env.NODE_ENV || 'development';
-  app.locals.ENV = env;
-  app.locals.ENV_DEVELOPMENT = env == 'development';
-  
-  app.set('views', config.root + '/app/views');
-  app.set('view engine', 'jade');
+module.exports = function (app, config) {
+  var env = process.env.NODE_ENV || 'development'
+  app.locals.ENV = env
+  app.locals.ENV_DEVELOPMENT = env === 'development'
+
+  app.set('views', config.root + '/app/views')
+  app.set('view engine', 'jade')
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
+  app.use(logger('dev'))
+  app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({
     extended: true
-  }));
-  app.use(cookieParser());
-  app.use(compress());
-  app.use(express.static(config.root + '/public'));
-  app.use(methodOverride());
+  }))
+  app.use(cookieParser())
+  app.use(compress())
+  app.use(express.static(config.root + '/public'))
+  app.use(methodOverride())
 
-  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+  var controllers = glob.sync(config.root + '/app/controllers/*.js')
   controllers.forEach(function (controller) {
-    require(controller)(app);
-  });
+    require(controller)(app)
+  })
+
+  app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+  })
 
   app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-  
-  if(app.get('env') === 'development'){
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+
+  if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
+      res.status(err.status || 500)
       res.render('error', {
         message: err.message,
         error: err,
         title: 'error'
-      });
-    });
+      })
+    })
   }
 
   app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-      });
-  });
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error'
+    })
+  })
 
-  return app;
-};
+  return app
+}
